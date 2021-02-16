bf62ac8c576ac9ec17f3f2648444aabdb77359bf
= <=>
2021-02-16 12:04:16 +0700

    Praktikum2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    Public function about ()
+    {
+        echo "NIM  : 1941720146; Nama : Annisa Wahyu Maulida";
+    }
+}
---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    Public function articles ()
+    {
+        echo "Halaman artikel dengan ID: ".$id;
+    }
+}
